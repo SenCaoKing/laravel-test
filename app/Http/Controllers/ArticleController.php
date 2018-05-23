b691d06cf5189f5eea2a2138d2af4e3ffa9e4257
SenCaoKing <1134349927@qq.com>
2018-05-23 10:15:14 +0800

    laravel学习之路由 --- 打印函数dump;Request all方法获取全部请求参数

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -56,15 +56,17 @@ class ArticleController extends Controller
      */
     public function edit(Request $request, $id, $name)
     {
-        $music = $request->input('music');
-        $book = request()->input('book');
-        $str = <<<php
-id:    $id <br>
-name:  $name <br>
-music: $music <br>
-book:  $book <br>
-php;
-        return $str; // 访问URL http://www.lar.test/article/edit/1/%E6%9B%B9%E5%8A%9F%E5%AE%89?music=%E6%9B%BE%E7%BB%8F%E7%9A%84%E4%BD%A0&book=%E5%B9%B3%E5%87%A1%E4%B8%96%E7%95%8C
+//        $music = $request->input('music');
+//        $book = request()->input('book');
+//        $str = <<<php
+//id:    $id <br>
+//name:  $name <br>
+//music: $music <br>
+//book:  $book <br>
+//php;
+//        return $str; // 访问URL http://www.lar.test/article/edit/1/%E6%9B%B9%E5%8A%9F%E5%AE%89?music=%E6%9B%BE%E7%BB%8F%E7%9A%84%E4%BD%A0&book=%E5%B9%B3%E5%87%A1%E4%B8%96%E7%95%8C
+
+        dump($request->all());
     }
 
     /**
