64e015838e1f062ed7fdc240de3fb10e0dbd15c6
SenCaoKing <1134349927@qq.com>
2018-05-23 15:15:02 +0800

    laravel学习之数据库填充Seeding --- DB类更安全、更易于维护地添加数据

--- app/Http/Controllers/DatabaseController.php
+++ app/Http/Controllers/DatabaseController.php
@@ -9,6 +9,29 @@ class DatabaseController extends Controller
 {
     public function insert()
     {
-        DB::insert('INSERT INTO articles (category_id,title,content) VALUEs (1, "文章1", "内容1")');
+        /**
+         * ①:
+         * 不提倡拼接字符串的方式添加数据(此种方式维护麻烦且不安全)
+         */
+        //DB::insert('INSERT INTO articles (category_id,title,content) VALUEs (1, "文章1", "内容1")');
+
+        /**
+         * ②:
+         */
+        DB::table('articles')->insert([
+            [
+                'category_id' => 2,
+                'title'        => '文章2',
+                'content'      => '内容2',
+            ],
+            [
+                'category_id' => 3,
+                'title'        => '文章3',
+                'content'      => '内容3',
+            ],
+        ]);
+
+
+
     }
 }
